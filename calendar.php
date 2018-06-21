788417fd4f7890d9b8ba78f4fcfa85f85260f6f7
YusongChang <soholifeguy@gmail.com>
2018-06-21 23:04:59 +0800

    修改年份按鈕顯示機制

--- calendar.php
+++ calendar.php
@@ -1,18 +1,18 @@
 <?php
 	require 'function.php';
 	
-	//取得當前年份
-	$pre_year = $next_year= $year = date('Y');
-	//取得當前月份
-	$month = date('m');	
+	//取得當前年份，並賦予上年下年 初始值 等於當前年份
+	$current_year  = date('Y');
+	//取得當前月份，並賦予上個月下個月 初始值 等於當前月份
+	$current_month = date('m');
 	//取得當月的天數
 	$month_day = date('t');
 	//取得當月的 1 號 是星期幾 0 ~ 7 表示
-	$week_day = date('w',mktime(0,0,0,$month,1,$year));
+	$week_day = date('w',mktime(0,0,0,$current_month,1,$current_year));
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="zh-TW">
 	<head>
 		<title></title>
 		<meta charset="UTF-8">
@@ -30,44 +30,75 @@
 	</head>
 	<body>
 		<div align='center'>
-			<p style="font-size:100px"><?php echo date('Y').' 年 '.date('n').' 月 '?></p>
-			<table>
-				<tr style="font-size:75px;">
-					<th style="color:red;">日</th>
-					<th>一</th>
-					<th>二</th>
-					<th>三</th>
-					<th>四</th>
-					<th>五</th>
-					<th>六</th>
-				</tr>
-				<?php 
-					if(isset($_GET['y'])){
-						if($_GET['y']=='pre'){
-							$pre_year--;
+			<?php 
+				if(isset($_GET['y']) && ($_GET['y'])!='') :
+					if($_GET['y']=='pre'):
+						//上年 故 初始年份 - 1
+						$pre_year = --$current_year;
+						//取得上一年 該月份 第 1 天是星期幾 0 ~ 7 表示
+						//註:這是年份往前算 ，月份不用改變
+						$mktime = date('w',mktime(0,0,0,$current_month,1,$pre_year));
+						$week_day = date('w',$mktime);
 
-						}
-						else if($_GET['y']=='next'){
-							
-						}
-					}
-					if(isset($_GET['m'])){
+					elseif($_GET['y']=='next'):
+						//下年 初始年份 + 1
+						$next_year = ++ $current_year; 
+						//取得下一年 該月份 第 1 天是星期幾 0 ~ 7 表示
+						//註:這是年份往後算 ，月份不用改變
+						$mktime = mktime(0,0,0,$current_month,1,$next_year);
+						$week_day = date('w',$mktime);
 
-					}
-					calendar_table($month_day, $week_day);
-				?>
-			</table>
+					else:	return false;
+					
+			?>
+					<?php endif;//第二層 if 結束 ?>
+
+					<p style="font-size:100px">
+						<?php echo $current_year.' 年 '. $current_month.' 月 ';?>
+					</p>
+					<table>
+						<tr style="font-size:75px;">
+							<th style="color:red;">日</th>
+							<th>一</th>
+							<th>二</th>
+							<th>三</th>
+							<th>四</th>
+							<th>五</th>
+							<th>六</th>
+						</tr>		
+					<?php
+						//處理完第二層 if 判斷 就 載入月曆
+						calendar_table($month_day, $week_day);
+					?>
+					</table>
+
+					
+			<?php else: ?>
+					<p style="font-size:100px">
+						<?php echo $current_year.' 年 '. $current_month.' 月 ';?>
+					</p>
+					<table>
+						<tr style="font-size:75px;">
+							<th style="color:red;">日</th>
+							<th>一</th>
+							<th>二</th>
+							<th>三</th>
+							<th>四</th>
+							<th>五</th>
+							<th>六</th>
+						</tr>
+				
+			<?php 
+				//預設網頁載入時，使用者沒有按任何按鈕，直接載入當前日期的月曆
+				calendar_table($month_day, $week_day); 
+				
+				endif;//第一層 if 結束;
+			?>
+					</table>
+				
 		</div>
-		<div align="center">
-			<p style="font-size:40px;">
-				<a href="?y=pre"><<</a>
-					<?php echo " ".$year." "."年 ";?>
-				<a href="?y=next">>></a>
-				<a style="padding-left:10px;" href="?m=pre"><</a>
-					<?php echo " ".$month." "."月 ";?>
-				<a href="?m=next">></a>
-			</p>	
-		</div>
+
+		<?php include 'menu.php' ?>
+
 	</body>
 </html>
-
---
+++ menu.php
@@ -0,0 +1,11 @@
+<!--上下年 上下月的 按鈕-->
+<div align="center">
+<p style="font-size:40px;">
+    <a href="?y=pre"><<</a>
+        <?php echo " ".$current_year." "."年 ";?>
+    <a href="?y=next">>></a>
+    <a style="padding-left:10px;" href="javascript:void(0);"><</a>
+        <?php echo " ".$current_month." "."月 ";?>
+    <a href="javascript:void(0);">></a>
+</p>	
+</div>
